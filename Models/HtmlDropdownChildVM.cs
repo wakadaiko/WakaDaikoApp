27b6eb6f479d4d8c72616dfcf52d8940877ac13c
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-05-28 20:03:11 -0700

    Added dropdown menu menus + changed workshop icon

---
+++ Models/HtmlDropdownChildVM.cs
@@ -0,0 +1,11 @@
+namespace WakaDaikoApp.Models
+{
+    public class HtmlDropdownChildVM
+    {
+        public string? Icon { get; set; }
+
+        public string? Heading { get; set; }
+
+        public string? Body { get; set; }
+    }
+}
