1946982a46f5f589bc2850d5bf3c1dc9d52aebf8
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-05-29 10:43:39 -0700

    Make account dropdown column when signed-in

--- Migrations/20240529031412_MySQL.Designer.cs
+++ Migrations/20240529031412_MySQL.Designer.cs
@@ -12,7 +12,7 @@ using WakaDaikoApp.Data;
 namespace WakaDaikoApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20240529025855_MySQL")]
+    [Migration("20240529031412_MySQL")]
     partial class MySQL
     {
         /// <inheritdoc />
